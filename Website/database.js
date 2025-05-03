584c6e2d945f25b0431460486d3450d84fb5cfa7
Rauna-H <raunahaishonga77@gmail.com>
2025-05-03 13:43:22 +0200

    Changed gitignore

--- Website/database.js
+++ Website/database.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
+  user: process.env.DB_USER || "Postgres",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_DATABASE || "kelotech",
-  password: process.env.DB_PASSWORD || "Jr71234567",
+  password: process.env.DB_PASSWORD || "Star99@99",
   port: process.env.DB_PORT || 5432,
 });
 
