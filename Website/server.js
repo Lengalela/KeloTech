b5ef999a16000d135de7db9394c8c652d592133f
Shami262005 <shamirav26@gmail.com>
2025-05-05 11:53:09 +0200

    PORT FIXED

--- Website/server.js
+++ Website/server.js
@@ -56,7 +56,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
